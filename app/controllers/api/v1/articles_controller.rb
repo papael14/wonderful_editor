09add380b273301ee827ad7a5808b15c85cc1f0f
yokoyama takuji <gato.02010417@gmail.com>
2020-11-24 05:11:26 +0900

    current_userをスタブ からdevise_token_authのヘルパーメソッドへ修正 base_api_controller で devise_token_auth で提供されている各種メソッドを使えるように修正 current_user, authenticate_user!, user_signed_in?

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,3 +1,69 @@
+module Api::V1
+  class ArticlesController < BaseApiController
+    # before_action :set_article, only: [:show, :update, :destroy]
+    before_action :authenticate_user!, only: [:create, :update, :destroy]
+
+    def index
+      articles = Article.order(updated_at: :desc)
+      render json: articles, each_serializer: Api::V1::ArticlePreviewSerializer
+    end
+
+    def show
+      # article = current_user.articles.find(params[:id])
+      article = Article.find(params[:id])
+      render json: article
+    end
+
+    def create
+      article = current_user.articles.create!(article_params)
+      render json: article
+    end
+
+    def update
+      article = current_user.articles.find(params[:id])
+      article.update!(article_params)
+      render json: article
+    end
+
+    def destroy
+      article = current_user.articles.find(params[:id])
+      article.destroy!
+    end
+
+    private
+
+      def set_article
+        # article = current_user.articles.find(params[:id])
+      end
+
+      def article_params
+        params.require(:article).permit(:title, :body)
+      end
+  end
+end
+
+# 作成中の試行錯誤メモ
+
+# def show
+#   # article = Article.find(params[:id])
+#   render json: article
+#   # render json: article, each_serializer: Api::V1::ArticleSerializer
+#   # each_serializerでSerializerを指定しなくても同じ階層のArticleSerializerから取得する
+# end
+
+# UserテーブルとArticleテーブルの紐付けは終わっているので
+# controllerではcurrent_userの下にarticleを作成すればOK
+# def create
+#   article = Article.new(
+#     title: params[:title],
+#     body: params[:body],
+#     user_id: params[:user_id],
+#   )
+#   article.save!
+#   article = Article.find(Article.last.id)
+#   render json: article
+# end
+
 # module Api::V1
 #   class ArticlesController < BaseApiController
 #     #before_action :set_article, only: [:show, :update, :destroy]
@@ -58,69 +124,4 @@
 #         params.require(:article).permit(:title, :body, :status, :user_id)
 #       end
 #   end
-# end
-module Api::V1
-  class ArticlesController < BaseApiController
-    # before_action :set_article, only: [:show, :update, :destroy]
-
-    def index
-      articles = Article.order(updated_at: :desc)
-      render json: articles, each_serializer: Api::V1::ArticlePreviewSerializer
-    end
-
-    def show
-      # article = Article.find(params[:id])
-      article = current_user.articles.find(params[:id])
-      render json: article
-    end
-
-    def create
-      article = current_user.articles.create!(article_params)
-      render json: article
-    end
-
-    def update
-      article = current_user.articles.find(params[:id])
-      article.update!(article_params)
-      render json: article
-    end
-
-    def destroy
-      article = current_user.articles.find(params[:id])
-      article.destroy!
-      # render json: article
-    end
-
-    private
-
-      def set_article
-        # @article = current_user.articles.find(params[:id])
-      end
-
-      def article_params
-        params.require(:article).permit(:title, :body)
-      end
-  end
-end
-
-# 作成中の試行錯誤メモ
-
-# def show
-#   # article = Article.find(params[:id])
-#   render json: article
-#   # render json: article, each_serializer: Api::V1::ArticleSerializer
-#   # each_serializerでSerializerを指定しなくても同じ階層のArticleSerializerから取得する
-# end
-
-# UserテーブルとArticleテーブルの紐付けは終わっているので
-# controllerではcurrent_userの下にarticleを作成すればOK
-# def create
-#   article = Article.new(
-#     title: params[:title],
-#     body: params[:body],
-#     user_id: params[:user_id],
-#   )
-#   article.save!
-#   article = Article.find(Article.last.id)
-#   render json: article
 # end
