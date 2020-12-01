3d7d8ad99973004449af99142402e3052a2530e2
yokoyama takuji <gato.02010417@gmail.com>
2020-12-02 06:02:53 +0900

    フロントエンドの繋ぎ込み３

---
+++ config/routes_bk3.rb
@@ -0,0 +1,24 @@
+# Rails.application.routes.draw do
+#   root to: "home#index"
+
+#   # reload 対策
+#   get "sign_up", to: "home#index"
+#   get "sign_in", to: "home#index"
+#   get "articles/new", to: "home#index"
+#   get "articles/:id", to: "home#index"
+
+#   namespace :api do
+#     namespace :v1 do
+#       mount_devise_token_auth_for "User", at: "auth", controllers: {
+#         registrations: "api/v1/auth/registrations",
+#       }
+#       namespace :articles do
+#         resources :drafts, only: [:index, :show]
+#       end
+#       namespace :current do
+#         resources :articles, only: [:index]
+#       end
+#       resources :articles
+#     end
+#   end
+# end
